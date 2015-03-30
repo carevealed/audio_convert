882c1a9439227851d055db0d799367ddda2f74f9
henryborchers <henryborchers@yahoo.com>
2015-03-30 16:11:57 -0700

    Moved location of file

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Audio Convert',
-    version='0.1',
+    name='CAVPP_Audio_Convert',
+    version='0.1.1',
     packages=find_packages(),
     # packages=['audio_convert', 'audio_convert.scripts', 'audio_convert.scripts.gui',
     #           'audio_convert.scripts.modules'],
     url='https://github.com/cavpp/audio_convert',
     install_requires=['OneSheet == 0.1.3.26'],
-    license='TBD',
+    license='GPL',
     author='Henry Borchers',
     author_email='hborcher@berkeley.edu',
-    entry_points={'console_scripts' : ['audioconvert = audio_convert.scripts.audioConvert:installed_start']},
+    entry_points={'console_scripts' : ['makemp3 = audio_convert.scripts.audioConvert:installed_start']},
     description='Converts wav files into MP3 files according to CAVPP standards.'
 )

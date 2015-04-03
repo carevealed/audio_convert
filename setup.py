be14a1a5439fc064e45d64b4952af140cf3ab12a
henryborchers <henryborchers@yahoo.com>
2015-04-03 13:38:16 -0700

    Added image

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='CAVPP_Audio_Convert',
-    version='0.1.2',
+    version='0.1.4',
     packages=find_packages(),
     url='https://github.com/cavpp/audio_convert',
     install_requires=['OneSheet == 0.1.3.26'],
     license='GPL',
     author='Henry Borchers',
     author_email='hborcher@berkeley.edu',
+    description='Converts wav files into MP3 files according to CAVPP standards.',
     entry_points={'console_scripts' : ['makemp3 = audio_convert.scripts.audioConvert:installed_start']},
-    description='Converts wav files into MP3 files according to CAVPP standards.'
+    package_data={"": ['audio_convert/scripts/gui/images/CAVPPcolor.gif']}
 )
